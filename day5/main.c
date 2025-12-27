a49db35c7f6392406a6cf089499ccd3b6d10aeeb
Marc Robison <marc@marcrobison.com>
2025-12-27 11:33:38 +0100

    feat(day5): find pages not in order

--- day5/main.c
+++ day5/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "ansi_color.h"
 	
 /*
  * Start by identifying which updates are in the correct order.
@@ -44,12 +45,22 @@ int main(void) {
 			break;
 		}
 	}
+	int order_rules_num[number_of_rules][2];
+	parse_file_into_rule_array(f, number_of_rules, rule_size, order_rules_num);
+	//for(int r = 0; r < number_of_rules; r++)
+	//{
+	//	printf("%d ", order_rules_num[r][0]);
+	//	printf("%d\n", order_rules_num[r][1]);
+	//}
+
 	char changes_data[maxsizeline];
 	// number of commas = amount of changes per line - 1
 	// numbers are 2 digits long
 	// number of numbers = strlen/3
+	int line = 0;
 	while(fgets(changes_data, maxsizeline, f) != NULL)
 	{
+		printf("rule line number %d\n", line++);
 		int numbers[strlen(changes_data)/3];
 		//printf("size of numbers: %d\n", sizeof(numbers)/sizeof(int));
 		char *pagestring;
@@ -61,17 +72,29 @@ int main(void) {
 		{
 			numbers[i++] = atoi(pagestring);
 		}
-		for(int a =0; a < (sizeof(numbers)/sizeof(int)); a++)printf("%d\n", numbers[a]);
-		printf("\n");
+		int number_of_numbers = sizeof(numbers)/sizeof(int);
+		//for(int a =0; a < number_of_numbers; a++)printf("%d\n", numbers[a]);
+		//printf("\n");
+		for(int n = 0; n < number_of_numbers; n++)
+		{
+			for (int r = 0; r < number_of_rules ; r++) {
+				int second_page_number = order_rules_num[r][1];
+
+				//printf("\t%d : %d\n", numbers[n], second_page_number);
+				if(numbers[n] == second_page_number)
+				{
+					printf("\tfound second rule\n");
+					for (int remaining = n+1; remaining < number_of_numbers; remaining++) {
+						printf("\t%d-%d\n", numbers[remaining], order_rules_num[r][0]);
+						if(numbers[remaining] == order_rules_num[r][0]) printf(RED"\tnot in order\n"reset);
+					}
+				}
+			
+			}
+		}
 	}
 
-	int order_rules_num[number_of_rules][2];
-	parse_file_into_rule_array(f, number_of_rules, rule_size, order_rules_num);
-	//for(int r = 0; r < number_of_rules; r++)
-	//{
-	//	printf("%d ", order_rules_num[r][0]);
-	//	printf("%d\n", order_rules_num[r][1]);
-	//}
+	
 	
 	fclose(f);
 	return 0;
