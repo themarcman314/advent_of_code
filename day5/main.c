2167fdc9c6909a9d07c81c54885353a5182bf239
Marc Robison <marc@marcrobison.com>
2025-12-28 01:31:10 +0100

    feat(day5): done with day5
    What is this giant pile of shit, please help.

--- day5/main.c
+++ day5/main.c
@@ -10,6 +10,8 @@
  * Add the middle page numbers up.
  * */
 
+#define PART2
+
 void parse_file_into_rule_array(FILE *f, int number_of_rules, int char_rule_size, int rules[number_of_rules][2]);
 
 int main(void) {
@@ -30,8 +32,7 @@ int main(void) {
 			sizeline = 0;
 		}
 	}
-	maxsizeline += 2; // include '\n' and '\0'
-	//printf("biggest line is %d characters\n", maxsizeline);
+	maxsizeline += 2; // include '\n' and '\0' printf("biggest line is %d characters\n", maxsizeline);
 	rewind(f);
 
 	char rule_data[7]; // 2x2 digit numbers, 1 seperator, 1 newline, and 1 NULL terminator
@@ -61,9 +62,10 @@ int main(void) {
 	int line = 0;
 	int sum = 0;
 	fgets(changes_data, maxsizeline, f); // skip over empty line
+	
 	while(fgets(changes_data, maxsizeline, f) != NULL)
 	{
-		int correct = true;
+		bool correct = true;
 		printf("rule line number %d\n", line++);
 		int numbers[strlen(changes_data)/3];
 		//printf("size of numbers: %d\n", sizeof(numbers)/sizeof(int));
@@ -79,31 +81,55 @@ int main(void) {
 		int number_of_numbers = sizeof(numbers)/sizeof(int);
 		for(int a =0; a < number_of_numbers; a++)printf("%d ", numbers[a]);
 		printf("\n");
-		for(int n = 0; n < number_of_numbers; n++)
-		{
-			for (int r = 0; r < number_of_rules ; r++) {
-				int second_page_number = order_rules_num[r][1];
 
-				//printf("\t%d : %d\n", numbers[n], second_page_number);
-				if(numbers[n] == second_page_number)
+			bool all_done = true;
+			int counter = 0;
+			do  {
+				all_done = true;
+				printf("counter : %d\n", counter++);
+				for(int n = 0; n < number_of_numbers; n++)
 				{
-					//printf("\tfound second rule\n");
-					for (int remaining = n+1; remaining < number_of_numbers; remaining++) {
-						//printf("\t%d-%d\n", numbers[remaining], order_rules_num[r][0]);
-						if(numbers[remaining] == order_rules_num[r][0]) {
-							correct = false;
-							printf(RED"\tnot in order\n"reset);
+					for (int r = 0; r < number_of_rules ; r++) {
+						int second_page_number = order_rules_num[r][1];
+
+						//printf("\t%d : %d\n", numbers[n], second_page_number);
+						if(numbers[n] == second_page_number)
+						{
+							//printf("\tfound second rule\n");
+							for (int remaining = n+1; remaining < number_of_numbers; remaining++) {
+								//printf("\t%d-%d\n", numbers[remaining], order_rules_num[r][0]);
+								if(numbers[remaining] == order_rules_num[r][0]) {
+									correct = false;
+									all_done = false;
+									printf("\tnot in order\n");
+									printf("\t%d should swap with %d\n",numbers[remaining], numbers[n]);
+									int temp = numbers[remaining];
+									numbers[remaining] = numbers[n];
+									numbers[n] = temp;
+									printf("\t%d swaped with %d\n",numbers[remaining], numbers[n]);
+								}
+							}
 						}
 					}
 				}
+					//if (number_of_correct == number_of_numbers);
+			}while(!all_done);
+
+			for(int a =0; a < number_of_numbers; a++)printf("%d ", numbers[a]);
+			printf("\n");
+#ifdef PART1
+			if (correct) {
+				printf("\tmiddle number: %d\n", numbers[number_of_numbers/2]);
+				sum += numbers[number_of_numbers/2];
 			}
+#endif
+#ifdef PART2
+			if (!correct) {
+				printf("\tmiddle number: %d\n", numbers[number_of_numbers/2]);
+				sum += numbers[number_of_numbers/2];
+			}
+#endif
 		}
-		if (correct) {
-			printf(GRN"\tmiddle number: %d\n"reset, numbers[number_of_numbers/2]);
-			sum += numbers[number_of_numbers/2];
-		
-		}
-	}
 	printf("total sum = %d\n", sum);
 
 	
